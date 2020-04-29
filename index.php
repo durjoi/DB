7ad4065bf4cb52d289bd4c6778b0194d82492aee
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-04-29 17:34:51 +0600

    Insert and Update method added

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
   require_once 'init.php';
   // $db = DB::getInstance()->action("SELECT *", "users", ['id', '=', 6])->first();
   // $db = DB::getInstance()->query("SELECT * FROM users WHERE id = ?", [6])->first();
-  $db = DB::getInstance()->get('users', ['id', '=', '6'])->first();
+  // $db = DB::getInstance()->get('users', ['id', '=', '6'])->first();
+  $db = DB::getInstance()->update('users', 7, [
+    'name' => 'Durjoi',
+    'email' => 'durjoi@durjoi.com',
+    'password' => 'passwd'
+  ]);
   print_r($db);
  ?>
